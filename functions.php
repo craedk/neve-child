d1b41109b49a25676ee2b7eb7d6087f991fa4306
craedk <casper@crae.dk>
2022-01-20 07:05:16 +0100

    Add square medium image size.

--- functions.php
+++ functions.php
@@ -15,3 +15,14 @@ add_action( 'wp_enqueue_scripts', 'neve_child_load_css', 20 );
 
 // Adding excerpt for page
 add_post_type_support( 'page', 'excerpt' );
+
+/**
+ * Add image sizes.
+ */
+if ( function_exists( 'add_image_size' ) ) {
+	add_image_size( 'crae-square-medium-image', 800, 800, true );
+}
+
+add_filter( 'crae_square_medium_image_size', function( $size ) {
+    return 'crae-square-medium-image';
+});
